39468da76b09699bafe7163ef7aa4a8784c3b8c2
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-09-24 14:37:45 +0200

    push övning1 färdig

--- lektion 2/ovning1.1/src/DatabaseConnection.java
+++ lektion 2/ovning1.1/src/DatabaseConnection.java
@@ -1,21 +1,73 @@
 import java.sql.*;
+
 public class DatabaseConnection {
     public static Connection connectDB() {
-        Connection con = null;
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:6306/it-sakerhet", "root", "");
-
-
-            Statement statement = con.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM users");
-
-            while (rs.next()) {
-                System.out.println("User: " + rs.getString("name"));
-            }
+            return DriverManager.getConnection("jdbc:mysql://localhost:6306/it-sakerhet", "root", "");
         } catch (Exception e) {
             System.out.println("Database connection error: " + e.getMessage());
+            return null;
         }
-        return con;
+
+    }
+
+    public static void addUser(String name, String password, String email) {
+        connectDB();
+        String query = "INSERT INTO users (name, password, email) VALUES (?,?,?)";
+        try {
+            PreparedStatement insertStmt = connectDB().prepareStatement(query);
+
+            insertStmt.setString(1, name);
+            insertStmt.setString(2, password);
+            insertStmt.setString(3, email);
+
+            int addUser = insertStmt.executeUpdate();
+
+            if (addUser > 0) {
+                System.out.println("User added: ");
+            } else {
+                System.out.println("Error!");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void showUser() throws SQLException {
+        connectDB();
+        String query = "SELECT * FROM users";
+        Statement statement = connectDB().createStatement();
+
+        ResultSet rs = statement.executeQuery(query);
+
+        while (rs.next()) {
+            System.out.println("-------------------");
+            System.out.println("Name: " + rs.getString("name"));
+            System.out.println("Password: " + rs.getString("password"));
+            System.out.println("Email: " + rs.getString("email"));
+        }
+    }
+
+    public static void deleteUser(String email) {
+        String query = "DELETE FROM users where email = ?";
+        try {
+            PreparedStatement deleteStmt = connectDB().prepareStatement(query);
+
+            deleteStmt.setString(1, email);
+
+            int deleteUser = deleteStmt.executeUpdate();
+
+            if (deleteUser > 0) {
+                System.out.println("User deleted: ");
+            } else {
+                System.out.println("Error!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- lektion 2/ovning1.1/src/Main.java
+++ lektion 2/ovning1.1/src/Main.java
@@ -1,16 +1,53 @@
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        Connection con = DatabaseConnection.connectDB();
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        while (choice != 4) {
+            System.out.println("1. Skapa konto");
+            System.out.println("2. Visa konto");
+            System.out.println("3. Radera konto");
+            System.out.println("4. Avsluta");
+
+            choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.println("Name:");
+                String name = scanner.next();
+
+                System.out.println("Password: ");
+                String password = scanner.next();
+
+                System.out.println("Email: ");
+                String email = scanner.next();
+
+                DatabaseConnection.addUser(name, password, email);
+            }
+
+            if (choice == 2) {
+                DatabaseConnection.showUser();
+            }
 
 
-        if (con != null) {
-            System.out.println("Anslutning till databasen lyckades!");
-        } else {
-            System.out.println("Misslyckades att ansluta till databasen.");
+            if (choice == 3) {
+                System.out.println("Write your email to delete your account: ");
+                String email = scanner.next();
+                DatabaseConnection.deleteUser(email);
+            }
+
+            if (choice == 4) {
+                System.out.println("Goodbye");
+            }
+
+            Connection con = DatabaseConnection.connectDB();
+
         }
-    }
 
+
+    }
 }
